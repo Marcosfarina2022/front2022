28136205635c5523f24ac03c06a8a41e08d433b1
Marcosfarina2022 <marcosfarina@tdf.edu.ar>
2022-07-25 16:14:37 -0300

    formulario con Java script

--- Clase  8- 22-07/script.js
+++ Clase  8- 22-07/script.js
@@ -1,1 +1,21 @@
-alert("Bienbenidos");
+"use strict";
+
+let btn_nombre = document.getElementById("btn-nombre");
+btn_nombre.addEventListener("click",mostrar);
+let btn_apellido = document.getElementById("btn-apellido");
+btn_apellido.addEventListener("click",mostrar);
+let btn_edad = document.getElementById("btn-edad");
+btn_edad.addEventListener("click",mostrar);
+
+function mostrar() {
+   let nombre;
+   let apellido;
+   let edad;
+   let inputNombre= document.getElementById("imputNombre")
+    nombre=inputNombre.value;
+   let inputApellido= document.getElementById("imputApellido")
+    apellido=inputApellido.value;
+   let inputEdad= document.getElementById("imputEdad")
+    edad=inputEdad.value;
+   alert("Datos Ingresados: "+" Nombre: " +nombre + " Apellido: " +apellido + " Edad: " +edad);
+}
