6e825344743e5f00be6807ac658c0b2b1a8757e3
Marcosfarina2022 <marcosfarina@tdf.edu.ar>
2022-09-12 13:32:41 -0300

    ejercicio de coordenadas

---
+++ ProgOrientadaObjetos/Clase2-ejercicioMicroondas/microondas.js
@@ -0,0 +1,44 @@
+// funcionamiento del Microondas
+var microondas = /** @class */ (function () {
+    //funcionalidades
+    function microondas(m, s, o) {
+        this.marca = m;
+        this.serie = s;
+        this.prendido = false;
+        this.opcion = o;
+    }
+    microondas.prototype.obtenerDatos = function () {
+        console.log("La marca del microondas es", this.marca, "y", "su numero de serie es:", this.serie);
+    };
+    microondas.prototype.prender = function () {
+        if (this.prendido == false) {
+            console.log("Debes prender el microondas para comenzar a utilizarlo");
+            this.prendido = true;
+            console.log("El dispositivo está prendido, el microondas está listo para utilizarlo");
+        }
+    };
+    microondas.prototype.programar = function () {
+        console.log("marcá la opción para calentar la comida", " ", this.opcion);
+    };
+    microondas.prototype.calentar = function () {
+        console.log("La comida ya empezó a calentarse");
+    };
+    return microondas;
+}());
+var micro = new microondas("samsungs", 37890456, 1);
+micro.obtenerDatos();
+micro.prender();
+micro.programar();
+micro.calentar();
+// funcionalidades
+//programar
+//calentar comida
+//cocinar comida
+//Descongelar comida
+//Estados
+// Prendido
+//Apagado
+//En reposo
+//marca
+//serie
+//opcion
---
+++ ProgOrientadaObjetos/Clase2-ejercicioMicroondas/microondas.ts
@@ -0,0 +1,55 @@
+// funcionamiento del Microondas
+
+class microondas {
+    //atributos
+    prendido: boolean;
+    marca: string;
+    serie:number;
+    opcion: number;
+    //funcionalidades
+    constructor(m: string, s:number, o:number) {
+        this.marca=m;
+        this.serie=s;
+        this.prendido= false;
+        this.opcion=o;   
+    }
+    obtenerDatos(){
+        console.log("La marca del microondas es",this.marca,"y","su numero de serie es:", this.serie);
+    }
+
+    prender(){
+        if (this.prendido==false) {
+            console.log("Debes prender el microondas para comenzar a utilizarlo");  
+            this.prendido= true;
+            console.log("El dispositivo está prendido, el microondas está listo para utilizarlo");
+        } 
+    }
+
+    programar(){
+        console.log("marcá la opción para calentar la comida"," ", this.opcion);
+    }    
+    calentar(){
+        
+        console.log("La comida ya empezó a calentarse");        
+    }    
+}
+
+let micro= new microondas("samsungs",37890456,1);
+micro.obtenerDatos();
+micro.prender();
+micro.programar();
+micro.calentar();
+
+// funcionalidades
+    //programar
+    //calentar comida
+    //cocinar comida
+    //Descongelar comida
+
+//Estados
+    // Prendido
+    //Apagado
+    //En reposo
+    //marca
+    //serie
+    //opcion
